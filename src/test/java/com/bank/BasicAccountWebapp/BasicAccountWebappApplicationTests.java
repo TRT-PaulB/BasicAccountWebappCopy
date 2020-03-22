21c6a403e4bf4a480ba75a2c4e8dc2662aff35d6
Paul Smith <paul.home47@gmail.com>
2020-03-22 18:27:56 +0000

    add spring boot project shell from initializer

---
+++ src/test/java/com/bank/BasicAccountWebapp/BasicAccountWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.BasicAccountWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAccountWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
